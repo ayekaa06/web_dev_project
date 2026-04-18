b8514c1d0a1980558d3be597afe0047c9b531b47
amirshadev <amirshadeev@mail.ru>
2026-04-18 14:04:25 +0500

    settings in back

--- agaai_backend/agaai_backend/settings.py
+++ agaai_backend/agaai_backend/settings.py
@@ -119,3 +119,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+        "KEY_PREFIX": "agaai",
+        "TIMEOUT": 3600,
+    }
+}
+REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_RATES": {
+        "import_user":  "10/hour",
+        "import_anon":  "3/hour",
+        "import_burst": "3/minute",
+    }
+}
